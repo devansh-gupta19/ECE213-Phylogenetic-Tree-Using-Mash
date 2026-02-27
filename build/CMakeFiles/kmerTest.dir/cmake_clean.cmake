5b1200572a0836fcd3de039d61cb7a89e5b122e0
aditya <adkamat@ucsd.edu>
2026-02-27 02:05:23 -0800

    read file

---
+++ build/CMakeFiles/kmerTest.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kmerTest.dir/src/gpuProperty.cu.o"
+  "CMakeFiles/kmerTest.dir/src/gpuProperty.cu.o.d"
+  "CMakeFiles/kmerTest.dir/src/kmer.cu.o"
+  "CMakeFiles/kmerTest.dir/src/kmer.cu.o.d"
+  "CMakeFiles/kmerTest.dir/src/main.cpp.o"
+  "CMakeFiles/kmerTest.dir/src/main.cpp.o.d"
+  "CMakeFiles/kmerTest.dir/src/murmur3Hash.cu.o"
+  "CMakeFiles/kmerTest.dir/src/murmur3Hash.cu.o.d"
+  "CMakeFiles/kmerTest.dir/src/twoBitCompressor.cpp.o"
+  "CMakeFiles/kmerTest.dir/src/twoBitCompressor.cpp.o.d"
+  "kmerTest"
+  "kmerTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/kmerTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
