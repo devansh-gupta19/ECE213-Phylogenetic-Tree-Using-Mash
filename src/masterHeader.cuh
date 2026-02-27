e89283245f6a17cbc73541f7df27b3abefb53f9b
devansh-gupta19 <devansh.gpt19@gmail.com>
2026-02-26 16:46:36 -0800

    added code for data transfer between device and host; complete code for kmer computation

--- src/masterHeader.cuh
+++ src/masterHeader.cuh
@@ -8,19 +8,31 @@
 #endif
 
 struct GpuAligner {
-    //std::vector<Sequence> seqs;
     int32_t longestLen;
-    int32_t numPairs;
+    //int32_t numPairs;
 
-    char*    d_seqs;
-    uint8_t* d_tb;
+    //char*    d_seqs;
+    uint32_t*    d_seqs;
     int32_t* d_seqLen;
-    int32_t* d_info;
+    int32_t *d_kmerSize;
+    size_t *d_kmerArr;
+    // int32_t* d_info;
 
     void seedTableOnGpu (
     uint32_t* compressedSeq,
     uint32_t seqLen,
     uint32_t kmerSize,
+    size_t* kmerArr);
+
+    void allocateMem(int32_t len, int32_t numKmers, int32_t kmerSize);
+    void transferData2Device(uint32_t *seq, int32_t len, int32_t kmerSize);
+    void transferData2Host(size_t *kmerArr, int32_t kmerArrSize);
+
+    void seedTableOnGpu (
+    uint32_t* compressedSeq,
+    uint32_t seqLen,
+    uint32_t kmerSize,
+    uint32_t numKmers,
     size_t* kmerArr);
 
     void MurmurHashCaller(const void* key, int len, uint32_t seed, uint32_t *hOut);
