e5c1277fc07eec77631e56e72a4ac8d13b031004
devansh-gupta19 <devansh.gpt19@gmail.com>
2026-02-28 01:22:02 -0800

    completed MASH sketch generation for given genome sequence

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # ECE213-Phylogenetic-Tree-Using-Mash
 ECE213-Phylogenetic-Tree-Using-Mash
+
+## Overview
+TODO
+
+## Setting up
+1. Clone the assignment repository in your HOME directory using the following example command and decompress the data files:
+```
+cd ~
+git clone https://github.com/devansh-gupta19/ECE260B-Dual-Core-ML-Accelerator.git
+cd ECE213-Phylogenetic-Tree-Using-Mash/dataset_dipper
+xz --decompress t1.aligned.fa.xz
+xz --decompress t2.unaligned.fa.xz
+cd ~
+```
--- src/masterHeader.cuh
+++ src/masterHeader.cuh
@@ -1,5 +1,5 @@
-#include <cstdint>  // Fixes uint32_t
-#include <cstddef>  // Fixes size_t
+#include <cstdint>
+#include <cstddef>
 #include <cstdio>
 
 // C++ compilers don't understand __global__, so we hide it if not using NVCC
@@ -16,27 +16,21 @@ struct GpuAligner {
     int32_t* d_seqLen;
     int32_t *d_kmerSize;
     size_t *d_kmerArr;
+    uint64_t *d_hashArr;
     // int32_t* d_info;
-
-    void seedTableOnGpu (
-    uint32_t* compressedSeq,
-    uint32_t seqLen,
-    uint32_t kmerSize,
-    size_t* kmerArr);
 
     void allocateMem(int32_t len, int32_t numKmers, int32_t kmerSize);
     void transferData2Device(uint32_t *seq, int32_t len, int32_t kmerSize);
     void transferData2Host(size_t *kmerArr, int32_t kmerArrSize);
-
-    void seedTableOnGpu (
+    void clearAndReset ();
+    uint32_t seedTableOnGpu (
     uint32_t* compressedSeq,
     uint32_t seqLen,
     uint32_t kmerSize,
     uint32_t numKmers,
     size_t* kmerArr);
 
-    void MurmurHashCaller(const void* key, int len, uint32_t seed, uint32_t *hOut);
-
+    void MurmurHashCaller(uint32_t numKmers, int kmerByteLen, uint32_t bottomK, uint64_t* hOut_sketch);
 };
 
 void printGpuProperties();
